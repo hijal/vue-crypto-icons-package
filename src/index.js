27eb3eaa6f956a57f3a7b0f50ac9aa6ed5be78c2
hijal <hijikeshhijal@gmail.com>
2022-07-22 08:42:01 +0600

    add ethereum, tether, bnb, usd coin, bitcoin lightning icon

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-import BTC from './components/BTC.vue';
+import BTC from "./components/BTC.vue";
+import ETH from "./components/ETH.vue";
+import Tether from "./components/Tether.vue";
+import USDC from "./components/USDC.vue";
+import Bitcoin_lightning from "./components/Bitcoin_lightning.vue";
+import BNB from "./components/BNB.vue";
 
-export { BTC };
+export { BTC, ETH, Tether, USDC, Bitcoin_lightning, BNB };
