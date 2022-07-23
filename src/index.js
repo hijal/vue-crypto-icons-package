471551e0c0a45a32a1787f76d12bddb9dfdeee17
hijal <hijikeshhijal@gmail.com>
2022-07-24 03:26:57 +0600

    add BUSD,   XRP,   ADA,   SOL,   DOGE,   DAI,   DOT,   MATIC,   AVAX,   SHIB,   TRX,   WBTC,   UNI,   LTC,   FTT,   ETC,   CRO

--- src/index.js
+++ src/index.js
@@ -2,7 +2,48 @@ import BTC from "./components/BTC.vue";
 import ETH from "./components/ETH.vue";
 import Tether from "./components/Tether.vue";
 import USDC from "./components/USDC.vue";
-import Bitcoin_lightning from "./components/Bitcoin_lightning.vue";
+import LBTC from "./components/Bitcoin_lightning.vue";
 import BNB from "./components/BNB.vue";
+import BUSD from "./components/BUSD.vue";
+import XRP from "./components/XRP.vue";
+import ADA from "./components/ADA.vue";
+import SOL from "./components/SOL.vue";
+import DOGE from "./components/DOGE.vue";
+import DAI from "./components/DAI.vue";
+import DOT from "./components/DOT.vue";
+import MATIC from "./components/MATIC.vue";
+import AVAX from "./components/AVAX.vue";
+import SHIB from "./components/SHIB.vue";
+import TRX from "./components/TRX.vue";
+import WBTC from "./components/WBTC.vue";
+import UNI from "./components/UNI.vue";
+import LTC from "./components/LTC.vue";
+import FTT from "./components/FTT.vue";
+import ETC from "./components/ETC.vue";
+import CRO from "./components/CRO.vue";
 
-export { BTC, ETH, Tether, USDC, Bitcoin_lightning, BNB };
+export {
+  BTC,
+  ETH,
+  Tether,
+  USDC,
+  LBTC,
+  BNB,
+  BUSD,
+  XRP,
+  ADA,
+  SOL,
+  DOGE,
+  DAI,
+  DOT,
+  MATIC,
+  AVAX,
+  SHIB,
+  TRX,
+  WBTC,
+  UNI,
+  LTC,
+  FTT,
+  ETC,
+  CRO,
+};
